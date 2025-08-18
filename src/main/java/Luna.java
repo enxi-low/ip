90f0aa507f82903b86fbd5c16a39ad704c61dfa4
enxi <194766557+enxi-low@users.noreply.github.com>
2025-08-19 01:21:25 +0800

    Add greeting and exit

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,4 +1,14 @@
 public class Luna {
     public static void main(String[] args) {
+        printSeparator();
+        System.out.println("Hello! I'm Luna.");
+        System.out.println("What can I do for you?");
+        printSeparator();
+        System.out.println("Bye. Hope to see you again soon!");
+        printSeparator();
+    }
+
+    public static void printSeparator() {
+        System.out.println("-------------------------------------");
     }
 }
