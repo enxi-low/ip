ecfa04e5c2f5a64f91cffd3bf874a4187fb3627e
enxi <194766557+enxi-low@users.noreply.github.com>
2025-08-19 01:30:50 +0800

    Implement echo commands

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class Luna {
     public static void main(String[] args) {
         printSeparator();
         System.out.println("Hello! I'm Luna.");
         System.out.println("What can I do for you?");
         printSeparator();
-        System.out.println("Bye. Hope to see you again soon!");
-        printSeparator();
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            printSeparator();
+            System.out.println(command);
+            printSeparator();
+        }
+        scanner.close();
     }
 
     public static void printSeparator() {
